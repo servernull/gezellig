7f1ed67534923b3ec3dc2378022ed551e2998819
Scott Lee Davis <skawtus@gmail.com>
2019-10-27 20:30:00 -0700

    parallel computation change

--- run.sh
+++ run.sh
@@ -4,21 +4,15 @@ URL=http://scottleedavis.com/
 
 echo $URL | faas-cli invoke openfaas-imagecrawler | jq > image_urls.txt
 
-cat image_urls.txt | faas-cli invoke openfaas-exiffeed | jq > image_objects.txt
+cat image_urls.txt | faas-cli invoke openfaas-exiffeed | jq > exif.txt
 
-input="./image_urls.txt"
-while IFS= read -r line
-do
-	line="${line%\"}"
-	line="${line#\"}"
-	echo $line | faas-cli invoke inception | jq 
-done < $input
+# input="./image_urls.txt"
+# while IFS= read -r line
+# do
+# 	line="${line%\"}"
+# 	line="${line#\"}"
+# 	echo $line | faas-cli invoke inception | jq 
+# done < $input
 
+cat image_urls.txt | faas-cli invoke openfaas-opennsfwfeed | jq > nsfw.txt
 
-input="./image_urls.txt"
-while IFS= read -r line
-do
-	line="${line%\"}"
-	line="${line#\"}"
-	echo $line | faas-cli invoke openfaas-opennsfw | jq 
-done < $input
